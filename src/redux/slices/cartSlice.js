094da970176d6806759a0bad950cdb2544030db3
Mostafa Jahani <mm.mostafa.mm@gmail.com>
2022-10-05 01:19:36 -0700

    save basket history in localStorage

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -4,7 +4,7 @@ import {createSlice} from '@reduxjs/toolkit'
 const cartSlice = createSlice({
     name: 'cart',
     initialState: {
-        list: []
+        list: localStorage.getItem('shopping-cart') ? JSON.parse(localStorage.getItem('shopping-cart')) : []
     },
     reducers: {
         addToCart: (state, action) => {
@@ -14,22 +14,28 @@ const cartSlice = createSlice({
                 state.list.map(p => p.id === action.payload.id ? {...p, qty: p.qty + 1} : p)
                 :
                 [...state.list, {...action.payload, qty: 1}]
+            localStorage.setItem('shopping-cart', JSON.stringify(state.list))
         },
         increment: (state, action) => {
             state.list = state.list.map(p => p.id === action.payload ? {...p, qty: p.qty + 1} : p)
+            localStorage.setItem('shopping-cart', JSON.stringify(state.list))
         },
         decrement: (state, action) => {
             const product = state.list.find(p => p.id === action.payload)
             state.list = product.qty > 1 ? state.list = state.list.map(p => p.id === action.payload ? {...p, qty: p.qty - 1} : p) : state.list
+            localStorage.setItem('shopping-cart', JSON.stringify(state.list))
         },
         deleteProduct: (state, action) => {
             state.list = state.list.filter(p => p.id !== action.payload)
+            localStorage.setItem('shopping-cart', JSON.stringify(state.list))
         },
         clearCart: (state, action) => {
             state.list = []
+            localStorage.setItem('shopping-cart', JSON.stringify(state.list))
         }
     }
 })
+
 
 
 export const {addToCart, increment, decrement, deleteProduct, clearCart} = cartSlice.actions
